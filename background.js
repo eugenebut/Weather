ef0b17abbdfa2d54d99f531cf9e668097a9d44e1
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2013-03-20 21:33:46 -0700

    Extracted Yahoo specific code to separate module.

--- background.js
+++ background.js
@@ -1,96 +1,28 @@
-var weatherLink = "";
-var temperature;
-var code;
-var timeout;
+var weatherLink = null;
+var timeout = null;
 
 function getWeather() {
-    var options = {"enableHighAccuracy": true, "timeout": 60000, "maximumAge": 0}
+    var options = {"enableHighAccuracy": false, "timeout": 60000, "maximumAge": 60000}
     navigator.geolocation.getCurrentPosition(getWeatherForLocation, getWeather, options);
 }
 
 
 function getWeatherForLocation(location) {
-    var locationRequest = new XMLHttpRequest();
-    var latituteAndLongitude = location.coords.latitude + ","  + location.coords.longitude;
-    var woeId = localStorage["location-coordinates" + latituteAndLongitude];
-    if (woeId) {
-        getWeatherForWoeID(woeId);
-        return;
+    var weatherRequest = new YahooWeatherRequest();
+
+    weatherRequest.onload = function(temperature, icon, link, ttl) {
+        chrome.browserAction.setBadgeText({text: temperature + "\u00B0" + getDegrees().toUpperCase()});
+        chrome.browserAction.setIcon({imageData: icon});
+        weatherLink = link || weatherLink;
+        timeout = window.setTimeout(getWeather, parseInt(ttl) * 1000);
     }
 
-    locationRequest.open("GET", "http://where.yahooapis.com/geocode?location=" + latituteAndLongitude + "&gflags=R&appid=zHgnBS4m");
-    locationRequest.onload = function() {
-        var woeIdNode = this.responseXML.getElementsByTagName("woeid")[0].childNodes[0];
-        if (!woeIdNode) {
-            // try to reload the weather again in 10 seconds
-            timeout = window.setTimeout(getWeather, 10000);
-            return;
-        }
-        var woeId = woeIdNode.nodeValue;
-        localStorage["location-coordinates" + latituteAndLongitude] = woeId;
-        getWeatherForWoeID(woeId);
-    }
-    locationRequest.send()
-}
-
-
-function getWeatherForWoeID(woeId) {
-    var weatherRequest = new XMLHttpRequest();
-    weatherRequest.open("GET", "http://weather.yahooapis.com/forecastrss?u=" + getDegrees() + "&w=" + woeId);
-    weatherRequest.onload = showWeather;
-    weatherRequest.onerror = function () {
+    weatherRequest.onerror = function() {
         // try to reload the weather again in 10 seconds
         timeout = window.setTimeout(getWeather, 10000);
     }
-    weatherRequest.send();
-}
 
-function showWeather() {
-    var channelNode = this.responseXML.getElementsByTagName("channel")[0];
-
-    // get link and ttl
-    var newWeatherLink = channelNode.getElementsByTagName("link")[0].childNodes[0].nodeValue;
-    if ("" != newWeatherLink) {
-        weatherLink = newWeatherLink
-    }
-    var ttl = channelNode.getElementsByTagName("ttl")[0].childNodes[0].nodeValue;
-
-    // get temperature and code
-    var itemNode = channelNode.getElementsByTagName("item")[0];
-    var conditionNode = itemNode.getElementsByTagNameNS("*", "condition")[0];
-    var newTemperature = conditionNode.attributes.getNamedItem("temp").nodeValue;
-    var newCode = conditionNode.attributes.getNamedItem("code").nodeValue;
-
-    // update temperature if necessary
-    if (temperature != newTemperature) {
-        chrome.browserAction.setBadgeText({text: newTemperature + "\u00B0" + getDegrees().toUpperCase()});
-        temperature = newTemperature;
-    }
-
-    // update image if necessary
-    if (code != newCode) {
-        var image = document.getElementById("image");
-        var canvas = document.getElementById("canvas");
-
-        image.onload = function() {
-            var context = canvas.getContext("2d");
-//            context.fillStyle = "white";
-//            context.fillRect(0,0,canvas.width, canvas.height);
-//            context.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-            context.drawImage(image, 0, 0, 27, 19);
-
-            var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-            chrome.browserAction.setIcon({imageData: imageData});
-        };
-        code = newCode;
-//        image.src = "http://l.yimg.com/a/i/us/we/52/" + code + ".gif";
-
-        image.src = "http://l.yimg.com/a/i/us/nws/weather/gr/" + code + "d.png";
-    }
-
-    // reload weather after cache is expired
-    timeout = window.setTimeout(getWeather, parseInt(ttl) * 1000);
+    weatherRequest.send(location, getDegrees());
 }
 
 
@@ -107,7 +39,6 @@ function onClick() {
 function onMessage(request, sender, sendResponse) {
     if (request.action == "reloadWeather") {
         clearTimeout(timeout);
-        temperature = "";
         getWeather();
     }
 }
---
+++ yahoo_weather.js
@@ -0,0 +1,75 @@
+function YahooWeatherRequest() {
+    this.onload = function() {};
+    this.onerror = function() {};
+}
+
+
+YahooWeatherRequest.prototype.send = function(location, degrees) {
+    // lets check if we cached location woeID
+    var latituteAndLongitude = location.coords.latitude + ","  + location.coords.longitude;
+    var storageKey = "location-coordinates" + latituteAndLongitude;
+    var woeID = localStorage[storageKey];
+    if (woeID) {
+        this._requestWeatherForWoeID(woeID);
+        return;
+    }
+
+    // lets find out location woid
+    var locationRequest = new XMLHttpRequest();
+    locationRequest.open("GET", "http://where.yahooapis.com/geocode?location=" + latituteAndLongitude + "&gflags=R&appid=zHgnBS4m");
+    locationRequest.onload = this._onLoadLocation.bind(this, locationRequest, storageKey);
+    locationRequest.onerror = this.onerror;
+    locationRequest.send();
+};
+
+
+YahooWeatherRequest.prototype._requestWeatherForWoeID = function(woeId) {
+    var weatherRequest = new XMLHttpRequest();
+    weatherRequest.open("GET", "http://weather.yahooapis.com/forecastrss?u=" + getDegrees() + "&w=" + woeId);
+    weatherRequest.onload = this._onLoadWeather.bind(this, weatherRequest);
+    weatherRequest.onerror = this.onerror;
+    weatherRequest.send();
+};
+
+
+YahooWeatherRequest.prototype._onLoadLocation = function(request, storageKey) {
+    var woeIdNode = request.responseXML.getElementsByTagName("woeid")[0].childNodes[0];
+    if (!woeIdNode) {
+        this.onerror();
+        return;
+    }
+
+    var woeId = woeIdNode.nodeValue;
+    localStorage[storageKey] = woeId;
+    this._requestWeatherForWoeID(woeId);
+}
+
+
+YahooWeatherRequest.prototype._onLoadWeather = function(request) {
+    var channelNode = request.responseXML.getElementsByTagName("channel")[0];
+
+    // get link and ttl
+    var link = channelNode.getElementsByTagName("link")[0].childNodes[0].nodeValue;
+    var ttl = channelNode.getElementsByTagName("ttl")[0].childNodes[0].nodeValue;
+
+    // get temperature and code
+    var itemNode = channelNode.getElementsByTagName("item")[0];
+    var conditionNode = itemNode.getElementsByTagNameNS("*", "condition")[0];
+    var temperature = conditionNode.attributes.getNamedItem("temp").nodeValue;
+    var code = conditionNode.attributes.getNamedItem("code").nodeValue;
+
+    // create image
+    var reportWeatherCallback = this.onload.bind();
+    var image = new Image();
+    image.onload = function() {
+        var canvas = document.createElement('canvas');
+        canvas.height = "19";
+        canvas.width = "19";
+        var context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, 27, 19);
+        var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        reportWeatherCallback(temperature, imageData, link, ttl);
+    }
+    image.onerror = this.onerror;
+    image.src = "http://l.yimg.com/a/i/us/nws/weather/gr/" + code + "d.png";
+}
