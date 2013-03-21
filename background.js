d7cf178fb42670652438a3038f83029d361bf261
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2013-03-20 22:15:54 -0700

    Fixed bindings.

--- background.js
+++ background.js
@@ -12,25 +12,25 @@ function WeatherUpdater() {
 WeatherUpdater.prototype.reload = function() {
     window.clearTimeout(this._timeout);
 
-    var options = {"enableHighAccuracy": false, "timeout": 60000, "maximumAge": 60000}
-    navigator.geolocation.getCurrentPosition(this._onLoadLocation, this.reload, options);
+    var options = {enableHighAccuracy: false, timeout: 60000, maximumAge: 60000}
+    navigator.geolocation.getCurrentPosition(this._onLoadLocation.bind(this), this.reload.bind(this), options);
 };
 
 
 WeatherUpdater.prototype._onLoadLocation = function(location) {
-    var weatherRequest = new YahooWeatherRequest();
+    var reloadCallback = function(timeout) {
+        this._timeout = window.setTimeout(this.reload.bind(this), timeout);
+    }
+    var reloadAfterTimeout = reloadCallback.bind(this)
 
+    var weatherRequest = new YahooWeatherRequest();
     weatherRequest.onload = function(temperature, icon, link, ttl) {
         chrome.browserAction.setBadgeText({text: temperature + "\u00B0" + getDegrees().toUpperCase()});
         chrome.browserAction.setIcon({imageData: icon});
         this.weatherLink = link || this.weatherLink;
-        this._timeout = window.setTimeout(getWeather, parseInt(ttl) * 1000);
+        reloadAfterTimeout(parseInt(ttl) * 1000);
     }
-
-    weatherRequest.onerror = function() {
-        this._timeout = window.setTimeout(getWeather, 10000);
-    }
-
+    weatherRequest.onerror = reloadCallback.bind(this, 10000);
     weatherRequest.send(location, getDegrees());
 }
 
@@ -42,7 +42,7 @@ function onInit() {
 
 
 function onClick() {
-    chrome.tabs.create({"url": WeatherUpdater().weatherLink}, null)
+    chrome.tabs.create({url: WeatherUpdater().weatherLink}, null)
 }
 
 
